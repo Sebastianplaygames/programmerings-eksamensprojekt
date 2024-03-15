18c27409a0a564f2d524a42e2016ebcf5518bf12
Sebastianpleygames <sebastianpleygames.dk@gmail.com>
2024-03-15 09:14:51 +0100

    class

--- sketch.js
+++ sketch.js
@@ -1,7 +1,20 @@
+function preload() {
+ // img = loadImage('skrivnavn.png');
+  
+  
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+card = new Card;
+hand = new Hand;
+player = new Player;
+dealer = new Dealer;
+
 }
 
 function draw() {
   background(42,100,25);
+  
+   
 }
