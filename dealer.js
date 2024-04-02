0190cdb6614d62ffa529cfa18c6d7fa29006283a
Sebastianpleygames <sebastianpleygames.dk@gmail.com>
2024-04-02 14:52:55 +0200

    noter

--- dealer.js
+++ dealer.js
@@ -1,9 +1,16 @@
 class Dealer{
 constructor(){
-
-    
+getHand = hand;
+addCardToHand = card;
 }
 
+// der dannes en if statement for at bestemme om dealer skal "hit" eller ej'
+/* if (totalValue() <= 17){
+dealer.hit()
+}
+else if (totalValue() > 17){
+dealer.stand()
 
-
+}
+*/
 }
