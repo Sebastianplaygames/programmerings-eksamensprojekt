f8e8470dd86cff0eac37b9e7fd56eeb69bf05fa6
Sebastianpleygames <sebastianpleygames.dk@gmail.com>
2024-05-01 17:42:30 +0200

    player edit v1.1

--- player.js
+++ player.js
@@ -1,14 +1,16 @@
 class Player{
-constructor(x,y,hand){
-this.x = x;
-this.y = y;
-this.getHand = hand;
-addCardToHand = card;
+constructor(x,y,getHand){
+this.x = X;
+this.y = Y;
+this.getHand = hand.hand;
+this.score = 0
 }
+
 update(){
+
 if (this.hit == true){
 this.getHand = this.getHand + addCardToHand
-
+this.getHand.push(card);
 }
 
 }
