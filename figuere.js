1e221395bae1b0ff0bb53d38c4697c9c9053fa6e
Jonathan Lunddorph <jona244s@edu.mercantec.dk>
2024-05-02 20:19:01 +0200

    Ny

--- figuere.js
+++ figuere.js
@@ -1,9 +1,5 @@
 class Figuere {
-  constructor() {}
-}
+  
 
-/*
-Tælle kort værdierne sammen
-Holde på kortene (gemme)
-    Gemme kortets individuelle værdi, så det kan vises på spillebrættet
-*/
+  
+}
